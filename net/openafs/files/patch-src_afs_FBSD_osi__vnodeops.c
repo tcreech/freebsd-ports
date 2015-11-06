5297e5ea3e4637f9c35078e60f83328065f44046
Tim Creech <tcreech@umd.edu>
2015-11-06 14:45:14 -0500

    openafs: translate VOP_ADVLOCK reqeusts better

--- net/openafs/files/patch-src_afs_FBSD_osi__vnodeops.c
+++ net/openafs/files/patch-src_afs_FBSD_osi__vnodeops.c
@@ -1,4 +1,4 @@
---- src/afs/FBSD/osi_vnodeops.c.orig	2015-09-16 19:39:46 UTC
+--- src/afs/FBSD/osi_vnodeops.c.orig	2015-10-28 19:22:26 UTC
 +++ src/afs/FBSD/osi_vnodeops.c
 @@ -79,7 +79,6 @@ static vop_mkdir_t	afs_vop_mkdir;
  static vop_mknod_t	afs_vop_mknod;
@@ -33,7 +33,15 @@
      {&vop_print_desc, (vop_t *) afs_vop_print},	/* print */
      {&vop_read_desc, (vop_t *) afs_vop_read},	/* read */
      {&vop_readdir_desc, (vop_t *) afs_vop_readdir},	/* readdir */
-@@ -1081,22 +1078,6 @@ afs_vop_ioctl(ap)
+@@ -592,6 +589,7 @@ afs_vop_create(ap)
+     GETNAME();
+ 
+     AFS_GLOCK();
++    ap->a_vap->va_mode |= S_IRUSR;
+     error =
+ 	afs_create(VTOAFS(dvp), name, ap->a_vap,
+ 		   ap->a_vap->va_vaflags & VA_EXCLUSIVE ? EXCL : NONEXCL,
+@@ -1081,22 +1079,6 @@ afs_vop_ioctl(ap)
      }
  }
  
@@ -56,3 +64,27 @@
  int
  afs_vop_fsync(ap)
       struct vop_fsync_args	/* {
+@@ -1589,12 +1571,21 @@ afs_vop_advlock(ap)
+ 				 * int  a_flags;
+ 				 * } */ *ap;
+ {
+-    int error;
++    int error, new_a_op;
+     struct ucred cr = *osi_curcred();
+ 
++    new_a_op = ap->a_op;
++    if(ap->a_op == F_UNLCK){
++	/* This makes no sense; I think passing F_UNLCK rather than
++	 * F_SETLCK in FreeBSD's kern_fcntl is a bug. (We certainly aren't
++	 * being asked to F_SETFD, which happens to equal F_UNLCK.) */
++        /* Quietly alter the erroneous op: */
++	new_a_op = F_SETLK;
++    }
++
+     AFS_GLOCK();
+     error =
+-	afs_lockctl(VTOAFS(ap->a_vp), ap->a_fl, ap->a_op, &cr, (int)ap->a_id);
++	afs_lockctl(VTOAFS(ap->a_vp), ap->a_fl, new_a_op, &cr, (int)ap->a_id);
+     AFS_GUNLOCK();
+     return error;
+ }
