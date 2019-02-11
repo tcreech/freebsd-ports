95e98b9878591e36a6d9baa2c31bef42968d8aa4
Tim Creech <tcreech@tcreech.com>
2019-02-10 21:23:37 -0500

    security/pam_krb5: hack in a patch for 12.0's busted pam_getenv

---
+++ security/pam_krb5/files/patch-cache.c
@@ -0,0 +1,18 @@
+--- cache.c.orig	2019-02-11 02:05:32 UTC
++++ cache.c
+@@ -45,8 +45,14 @@ pamk5_get_krb5ccname(struct pam_args *args, const char
+         name = getenv(key);
+     if (name == NULL || *name == '\0')
+         return NULL;
+-    else
++    else {
++        /* FreeBSD 12 pulled in changes from OpenPAM which seem to include
++         * adding a bug to pam_getenv. In particular, it always includes the
++         * '=' character at the beginning of the result. Hack in a fix. */
++        if (name[0] == '=')
++            name++;
+         return name;
++    }
+ }
+ 
+ 
