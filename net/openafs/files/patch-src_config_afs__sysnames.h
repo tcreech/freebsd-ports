55aedc930f88f2b16b3b6042aac1cae7e40882a7
Tim Creech <tcreech@tcreech.com>
2026-06-27 23:41:06 -0400

    net/openafs: update to 1.8.16.1
    Patches match contents of 14206_3_fbsd15_20260627_18_16_1 branch.
    
    Also, add support for 15.1.

--- net/openafs/files/patch-src_config_afs__sysnames.h
+++ net/openafs/files/patch-src_config_afs__sysnames.h
@@ -1,4 +1,4 @@
---- src/config/afs_sysnames.h.orig	2026-02-26 17:15:49 UTC
+--- src/config/afs_sysnames.h.orig	2026-06-25 20:03:19 UTC
 +++ src/config/afs_sysnames.h
 @@ -262,6 +262,12 @@
  #define SYS_NAME_ID_i386_fbsd_121      2161
@@ -13,7 +13,7 @@
  
  #define SYS_NAME_ID_ia64_linux2		2200
  #define SYS_NAME_ID_ia64_linux22	2201
-@@ -380,6 +386,21 @@
+@@ -380,6 +386,23 @@
  #define SYS_NAME_ID_amd64_fbsd_121     3061
  #define SYS_NAME_ID_amd64_fbsd_122     3062
  #define SYS_NAME_ID_amd64_fbsd_123     3063
@@ -25,6 +25,7 @@
 +#define SYS_NAME_ID_amd64_fbsd_142     3072
 +#define SYS_NAME_ID_amd64_fbsd_143     3073
 +#define SYS_NAME_ID_amd64_fbsd_150     3080
++#define SYS_NAME_ID_amd64_fbsd_151     3081
 +
 +#define SYS_NAME_ID_arm64_fbsd_132     3200
 +#define SYS_NAME_ID_arm64_fbsd_140     3210
@@ -32,6 +33,7 @@
 +#define SYS_NAME_ID_arm64_fbsd_142     3212
 +#define SYS_NAME_ID_arm64_fbsd_143     3213
 +#define SYS_NAME_ID_arm64_fbsd_150     3220
++#define SYS_NAME_ID_arm64_fbsd_151     3221
  
  #define SYS_NAME_ID_amd64_w2k           3400
  
